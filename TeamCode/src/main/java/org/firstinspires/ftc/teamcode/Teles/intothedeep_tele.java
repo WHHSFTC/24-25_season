606dd66bbcc2a6e1cb1cb64795026482bd495097
abhigoel25 <89696508+abhigoel25@users.noreply.github.com>
2024-10-19 15:41:53 -0400

    into the deep dt tele works

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teles/intothedeep_tele.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teles/intothedeep_tele.java
@@ -22,13 +22,25 @@ public class intothedeep_tele extends OpMode{
     DcMotor lf;
     DcMotor rb;
     DcMotor lb;
-//
+    DcMotor extendo;
+    DcMotor rs;
+    DcMotor ls;
+
+    ElapsedTime timer = new ElapsedTime(ElapsedTime.Resolution.MILLISECONDS);
+    ElapsedTime totalRunTime = new ElapsedTime(ElapsedTime.Resolution.MILLISECONDS);
+    double timePerLoop;
+    double loopCumulativeTime = 0.0;
+    double loopCounter = 0.0;
+
     @Override
     public void init(){
         rf = hardwareMap.get(DcMotor.class, "motorRF");
         lf = hardwareMap.get(DcMotor.class, "motorLF");
         rb = hardwareMap.get(DcMotor.class, "motorRB");
         lb = hardwareMap.get(DcMotor.class, "motorLB");
+        rs = hardwareMap.get(DcMotor.class, "rs");
+        ls = hardwareMap.get(DcMotor.class, "ls");
+        extendo = hardwareMap.get(DcMotor.class, "extendo");
 
         rf.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         lf.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
@@ -38,11 +50,25 @@ public class intothedeep_tele extends OpMode{
 
     //@Override
     public void start(){
-
+        extendo.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rs.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        ls.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
     }
 
     @Override
     final public void loop(){
+
+        timePerLoop = timer.milliseconds();
+        timer.reset();
+        loopCounter++;
+        loopCumulativeTime += timePerLoop;
+
+        if (loopCumulativeTime >= 1000) {
+            telemetry.addData("Time per Loop", "Time per Loop: " + loopCumulativeTime/loopCounter);
+            loopCounter = 0.0;
+            loopCumulativeTime = 0.0;
+        }
+
         double y = -gamepad1.left_stick_x; //verticals
         double x = -gamepad1.left_stick_y; //horizontal
         double r = -gamepad1.right_stick_x; //pivot and rotation
@@ -63,6 +89,12 @@ public class intothedeep_tele extends OpMode{
         lf.setPower(preLF/max);
         rb.setPower(preRB/max);
         lb.setPower(preLB/max);
+
+
+        telemetry.addData("rs position", rs.getCurrentPosition());
+        telemetry.addData("ls position", ls.getCurrentPosition());
+        telemetry.addData("time per loop", timePerLoop);
+        telemetry.update();
     }
 
     @Override
