fc3fe59adc8bc9013223adca89dcda3d63aa8a08
abhigoel25 <89696508+abhigoel25@users.noreply.github.com>
2024-10-19 00:07:10 -0400

    initial dt tele for into the deep

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teles/intothedeep_tele.java
@@ -0,0 +1,75 @@
+package org.firstinspires.ftc.teamcode.Teles;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.TouchSensor;
+import com.qualcomm.robotcore.hardware.VoltageSensor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import java.util.List;
+
+@Disabled
+@TeleOp
+abstract public class intothedeep_tele extends OpMode{
+
+    DcMotor rf;
+    DcMotor lf;
+    DcMotor rb;
+    DcMotor lb;
+
+    @Override
+    public void init(){
+        rf = hardwareMap.get(DcMotor.class, "motorRF");
+        lf = hardwareMap.get(DcMotor.class, "motorLF");
+        rb = hardwareMap.get(DcMotor.class, "motorRB");
+        lb = hardwareMap.get(DcMotor.class, "motorLB");
+
+        rf.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        lf.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rb.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        lb.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    }
+
+    @Override
+    public void start(){
+
+    }
+
+    @Override
+    final public void loop(){
+        double y = -gamepad1.left_stick_x; //verticals
+        double x = -gamepad1.left_stick_y; //horizontal
+        double r = -gamepad1.right_stick_x; //pivot and rotation
+        double scalar = 1.0;
+
+        if(gamepad1.left_bumper){
+            scalar = 0.6;
+        }
+
+        double preRF = r*scalar + y*scalar + x*scalar;
+        double preLF = r*scalar + y*scalar - x*scalar;
+        double preRB = r*scalar -y*scalar + x*scalar;
+        double preLB = r*scalar -y*scalar -x*scalar;
+
+        double max = Math.max(Math.max(Math.max(Math.max(preRF, preRB), preLB), preLF), 1);
+
+        rf.setPower(preRF/max);
+        lf.setPower(preLF/max);
+        rb.setPower(preRB/max);
+        lb.setPower(preLB/max);
+    }
+
+    @Override
+    public void stop(){
+        rf.setPower(0);
+        lf.setPower(0);
+        rb.setPower(0);
+        lb.setPower(0);
+    }
+}
